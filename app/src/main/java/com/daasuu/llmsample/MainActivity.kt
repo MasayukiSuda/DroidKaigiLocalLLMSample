2c90237d7d58940ccdf4615eb7a32b369ec25010
Suda Masayuki[ 須田雅之 ] <masayuki.suda@linecorp.com>
2025-08-05 14:39:33 +0900

    Initial Commit

---
+++ app/src/main/java/com/daasuu/llmsample/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.daasuu.llmsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.daasuu.llmsample.ui.theme.DroidKaigiLocalLLMSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DroidKaigiLocalLLMSampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DroidKaigiLocalLLMSampleTheme {
+        Greeting("Android")
+    }
+}
